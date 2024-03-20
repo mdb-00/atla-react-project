88bb017758bbf831968584f94004b111b406c57d
Bri-Griggs <hjackman07@gmail.com>
2024-03-20 14:06:58 -0500

    added images/edited header

---
+++ atla/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+// import images from "../../images/fontbolt.png";
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <img src="../../images/fontbolt.png"></img>
+        <p>AVATAR: The Last Airbender Project</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ atla/src/Components/MainContent/MainContent.jsx
@@ -0,0 +1,14 @@
+function MainContent() {
+    return (
+      <main className= "mainContent">
+        {/* <img src="../../images/wan shi tong.png"></img> */}
+        Long ago, the 4 nations lived together in harmony.
+        But then everything changed when the fire nation
+        attacked. Only the Avatar, master of all elements
+        could stop them. But when the world needed him most,
+        he vanished.
+      </main>
+    );
+  }
+  
+  export default MainContent;
