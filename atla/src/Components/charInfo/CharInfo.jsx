c051179d6892c2f1341923e5c35dd93943dc671c
Kelvin Owens <147459916+KelvinOwens1@users.noreply.github.com>
2024-03-24 23:58:55 -0500

    Update CharInfo.jsx

--- atla/src/Components/charInfo/CharInfo.jsx
+++ atla/src/Components/charInfo/CharInfo.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export default function CharInfo() {
     return <h1>This is the individual Character Info - Coming Soon</h1>
     
-=======
-export default function CharInfo() {
-    return <h1>This is the individual Character Info - Coming Soon</h1>
-    
->>>>>>> 9f2d159015c072a9fec91fd4bfc2a085bf9a191d
 }
