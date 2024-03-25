840bcb7689d0ea8211eeb13b18454f5edfeaf542
Kelvin Owens <147459916+KelvinOwens1@users.noreply.github.com>
2024-03-24 23:48:20 -0500

    Update App.jsx

--- atla/src/App.jsx
+++ atla/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import MainContent from "./Components/MainContent/MainContent";
+import Navbar from "./Components/nav/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
     function App() {
       return (
-        
+        <Router>
           <div className="container">
               <Header />
+              <Navbar />
               <MainContent />
               <Footer />
           </div>
+        </Router>
       )
   }
   
