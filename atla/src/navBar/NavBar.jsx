53d2c5f75a200f94f6817632b52a1ae466b5505a
Kelvin Owens <147459916+KelvinOwens1@users.noreply.github.com>
2024-03-25 21:55:01 -0500

    Update NavBar.jsx

--- atla/src/navBar/NavBar.jsx
+++ atla/src/navBar/NavBar.jsx
@@ -1,9 +1,3 @@
-// import { useState } from "react";
-// import styles from "../App.css";
-// import AddCharacter from "../Components/AddCharacter/AddCharacter";
-// import DeleteCharacter from "../Components/DeleteCharacter/DeleteCharacter";
-// import CharacterCards from "../Components/CharacterCards/CharacterCards";
-
 function NavBar() {
 
     return <nav className="nav">
@@ -12,44 +6,7 @@ function NavBar() {
             <li><a href="/" className="site-title">Home</a></li>
             <li><a href="/CharacterCards/CharacterCards">Character List</a></li>
             <li><a href="/AddCharacter/AddCharacter">Add Character</a></li>
-            <li><a href="/DeleteCharacter/DeleteCharacter">Delete Character</a></li>
-            <li><a href="/charInfo/CharInfo">Character Info</a></li>
         </ul>
     </nav>
-
-//     const[isActive, setIsActive] = useState(false);
-//     const toggleActiveClass = () => {
-//         setIsActive(!isActive);
-//     };
-//     const removeActive = () => {
-//         setIsActive(false)
-//     }
-
-//     return (
-//         <div className="App">
-//       <header className="App-header">
-//         <nav className={`${styles.navbar}`}>
-//           {/* logo */}
-//           <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
-//             <li onClick={removeActive}>
-//               <a href='/' className={`${styles.navLink}`}>Home</a>
-//             </li>
-//             <li onClick={removeActive}>
-//               <a href='AddCharacter' className={`${styles.navLink}`}>Add Character</a>
-//             </li>
-//             <li onClick={removeActive}>
-//               <a href='DeleteCharacter' className={`${styles.navLink}`}>Delete Character</a>
-//             </li>
-//             <li onClick={removeActive}>
-//               <a href='CharacterCards' className={`${styles.navLink}`}>Characters</a>
-//             </li>
-//           </ul>
-//         </nav>
-//       </header>
-//     </div>
-//   );
 }
-    
-
- 
 export default NavBar;
